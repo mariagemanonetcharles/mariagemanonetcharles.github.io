e2b6c5bbb09af4d1e527606080fd57eb7bee5360
Manon Deloupy <mdeloupy@gmail.com>
2022-02-11 21:23:46 -0500

    Add map

---
+++ assets/javascript/main.js
@@ -0,0 +1,12 @@
+// Initialize and add the map
+function initMap() {
+    const home = { lat: 45.23207409855136, lng: -73.31837062403082 }
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 11,
+        center: home,
+    });
+    const marker = new google.maps.Marker({
+        position: home,
+        map: map,
+    });
+}
